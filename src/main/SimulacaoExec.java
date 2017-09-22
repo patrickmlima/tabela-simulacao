01f7ed573ef60c5788c98bb77b6df1e94a1108f9
Patrick Lima <patrickm.lima1@gmail.com>
2017-09-22 11:27:22 -0400

    Gerando simulações para vários dias baseados numa quantidade de horas

--- src/main/SimulacaoExec.java
+++ src/main/SimulacaoExec.java
@@ -1,12 +1,25 @@
 package main;
 
+import java.io.File;
+
 import model.TabelaSimulacao;
 
 public class SimulacaoExec {
-	// definir parâmetros
 	public static void main(String args[]) {
-		// construir objeto da tabela e executar
+		int TOTAL_DIAS = 20;
+		float MINUTOS_DIA = (18 - 8) * 60f;
+		
 		TabelaSimulacao simulacao = new TabelaSimulacao();
-		simulacao.gerarSimulacao(10, 15, 9, 11, 300);
+		
+		for(int i = 0; i < TOTAL_DIAS; ++i) {
+			try {
+				File file = new File("resources/simulacao_" + (i + 1) + ".txt");
+				System.out.println("saved in: " + file.getAbsolutePath());
+				simulacao.gerarSimulacao(10f, 15f, 9f, 13f, MINUTOS_DIA, file);
+				System.out.println("-----------------------------------------------------\n");
+			} catch(Exception e) {
+				
+			}
+		}
 	}
 }
--- src/model/TabelaSimulacao.java
+++ src/model/TabelaSimulacao.java
@@ -1,20 +1,34 @@
 package model;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TabelaSimulacao {
 	private ArrayList<LinhaSimulacao> linhas = new ArrayList<LinhaSimulacao>();
 	
+	private Float tempoServicoTotal = 0f;
+	
+	private Float tempoClienteFilaTotal = 0f;
+	
+	private Float tempoClienteSistemaTotal = 0f;
+	
+	private Float tempoLivreOperadorTotal = 0f;
+	
+	
 	public TabelaSimulacao() {
 		// construir lista de linhas
 	}
 	
-	public void gerarSimulacao(Integer maxTEC, Integer maxTS, Integer tempoSimulado) {
-		gerarSimulacao(0, maxTEC, 0, maxTS, tempoSimulado);
+	public void gerarSimulacao(Float maxTEC, Float maxTS, Float tempoSimulado) {
+		gerarSimulacao(0f, maxTEC, 0f, maxTS, tempoSimulado);
 	}
 	
-	public void gerarSimulacao(Integer minTEC, Integer maxTEC, Integer minTS, Integer maxTS, Integer tempoSimulado) {
+	public void gerarSimulacao(Float minTEC, Float maxTEC, Float minTS, Float maxTS, Float tempoSimulado) {
+		reset();
+		
 		LinhaSimulacao prev = null;
 		while(true) {
 			LinhaSimulacao linha = new LinhaSimulacao(minTEC, maxTEC, minTS, maxTS);
@@ -22,19 +36,70 @@ public class TabelaSimulacao {
 			linhas.add(linha);
 			prev = linha;
 			
-			System.out.println(linha.getTempoUltimaChegada() + "; " + linha.getTempoChegadaRelogio() + "; " + 
-			linha.getTempoServico() + "; " + linha.getTempoInicioServico() + "; " + linha.getTempoClienteFila() + "; " +
-					linha.getTempoFinalServico() + "; " + linha.getTempoClienteSistema() + "; " + linha.getTempoOciosoOperador());
+			System.out.println(
+					linha.getTempoUltimaChegada() + "; " + linha.getTempoChegadaRelogio() + "; " + 
+					linha.getTempoServico() + "; " + linha.getTempoInicioServico() + "; " + 
+					linha.getTempoClienteFila() + "; " + linha.getTempoFinalServico() + "; " + 
+					linha.getTempoClienteSistema() + "; " + linha.getTempoOciosoOperador()
+					);
+			
+			tempoServicoTotal += linha.getTempoServico();
+			tempoClienteFilaTotal += linha.getTempoClienteFila();
+			tempoClienteSistemaTotal += linha.getTempoClienteSistema();
+			tempoLivreOperadorTotal += linha.getTempoOciosoOperador();
+			
 			if(linha.getTempoChegadaRelogio() > tempoSimulado) {
 				break;
 			}
-			
 		}
+		
+		System.out.println("Tempo de servico total: " + tempoServicoTotal);
+		System.out.println("Tempo do cliente na fila: " + tempoClienteFilaTotal);
+		System.out.println("Tempo do cliente no sistema: " + tempoClienteSistemaTotal);
+		System.out.println("Tempo livre operador: " + tempoLivreOperadorTotal);
+	}
+	
+	public void gerarSimulacao(Float minTEC, Float maxTEC, Float minTS, Float maxTS, 
+			Float tempoSimulado, File output) throws IOException {
+		reset();
+		FileWriter writer = new FileWriter(output);
+		
+		LinhaSimulacao prev = null;
+		while(true) {
+			LinhaSimulacao linha = new LinhaSimulacao(minTEC, maxTEC, minTS, maxTS);
+			linha.setupData(prev);
+			linhas.add(linha);
+			prev = linha;
+			
+			String resultStr = 
+					linha.getTempoUltimaChegada() + "; " + linha.getTempoChegadaRelogio() + "; " + 
+					linha.getTempoServico() + "; " + linha.getTempoInicioServico() + "; " + 
+					linha.getTempoClienteFila() + "; " + linha.getTempoFinalServico() + "; " + 
+					linha.getTempoClienteSistema() + "; " + linha.getTempoOciosoOperador() + "\n";
+			
+			writer.write(resultStr);
+			
+			tempoServicoTotal += linha.getTempoServico();
+			tempoClienteFilaTotal += linha.getTempoClienteFila();
+			tempoClienteSistemaTotal += linha.getTempoClienteSistema();
+			tempoLivreOperadorTotal += linha.getTempoOciosoOperador();
+			
+			if(linha.getTempoChegadaRelogio() > tempoSimulado) {
+				break;
+			}
+		}
+		
+		System.out.println("Tempo de servico total: " + tempoServicoTotal);
+		System.out.println("Tempo do cliente na fila: " + tempoClienteFilaTotal);
+		System.out.println("Tempo do cliente no sistema: " + tempoClienteSistemaTotal);
+		System.out.println("Tempo livre operador: " + tempoLivreOperadorTotal);
+		writer.flush();
+		writer.close();
 	}
 	
 	public void gerarTeste() {
-		int tec[] = {15, 12, 10, 10, 12, 15, 10, 12, 10, 10, 10, 12, 15, 12, 12};
-		int ts[] = {11, 10, 9, 10, 9, 10, 11, 9, 11, 10, 11, 9, 10, 9, 11};
+		float tec[] = {15, 12, 10, 10, 12, 15, 10, 12, 10, 10, 10, 12, 15, 12, 12};
+		float ts[] = {11, 10, 9, 10, 9, 10, 11, 9, 11, 10, 11, 9, 10, 9, 11};
 		LinhaSimulacao prev = null;
 		for(int i = 0; i < 15; i++) {
 			LinhaSimulacao linha = new LinhaSimulacao(tec[i], ts[i]);
@@ -55,5 +120,10 @@ public class TabelaSimulacao {
 		this.linhas = linhas;
 	}
 	
-	
+	private void reset() {
+		tempoServicoTotal = 0f;
+		tempoClienteFilaTotal = 0f;
+		tempoClienteSistemaTotal = 0f;
+		tempoLivreOperadorTotal = 0f;
+	}
 }
